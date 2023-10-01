ae97ccb931c5a080a082281660136ed42a5fa353
javafs <alearningbee@outlook.com>
2023-09-30 22:50:15 -0700

    updated

--- src/Point.java
+++ src/Point.java
@@ -1,5 +1,10 @@
+/*
+   A Point object stores the (x, y) coordinates of a position in two dimensional space.
+   See textbook pages 539 - 567 for various versions
+ */
 public class Point {
-   private int x, y;
+   private int x;
+   private int y;
 
    // default constructor
    public Point(){
@@ -7,20 +12,32 @@ public class Point {
       this.y = 0;
    }
 
-   // parametric constructor
+   /*
+    this is the default constructor from textbook page 564, which is equivalent to above
+   public Point() {
+      this(0, 0); // calls Point(int, int) constructor
+   }
+   */
+
+   // parametric constructor with one parameter only
+   // this is not a useful constructor but only for demo purpose here
+   // note that the initial value of x is 0
    public Point(int y){
       this.y = y;
    }
 
-   // parametric constructor
+   // parametric constructor (with two parameters)
    public Point(int x, int y){
       this.x = x;
       this.y = y;
    }
 
+   // returns the x-coordinate of this point
    public int getX(){
       return x;
    }
+
+   // returns the y-coordinate of this point
    public int getY(){
       return y;
    }
@@ -33,23 +50,51 @@ public class Point {
       this.y = y;
    }
 
-   public void setLocation(int newX, int newY){
-      x = newX;
-      y = newY;
+   // sets this point's (x, y) location to the given values
+   public void setLocation(int x, int y){
+      this.x = x; // note that 'this' must be used differentiate the two x's
+      this.y = y; // note that 'this' must be used differentiate the two y's
    }
 
+   // sets a point's (x, y) location to the given values
+   public static void setLocationS(Point p, int x, int y){
+      p.x = x;
+      p.y = y;
+   }
+
+   // shifts this point's location by the given amount
    public void translate(int dx, int dy){
-      x += dx;
-      y += dy;
+      this.x += dx;
+      this.y += dy;
    }
 
+   // shifts a point's location by the given amount
+   public static void translateS(Point p, int dx, int dy){
+      p.x += dx;
+      p.y += dy;
+   }
+
+   // returns the distance between this Point and (0, 0)
+   public double distanceFromOrigin() {
+      return Math.sqrt(x * x + y * y);
+   }
+
+   // returns the distance between this Point and other Point
    public double distance (Point other){
       double dx = this.x - other.x;
       double dy = this.y - other.y;
       return Math.sqrt(dx*dx + dy*dy);
    }
+
+   // this static methods returns the distance between two given Points
+   public static double distanceBetweenTwoPoints(Point p1, Point p2){
+      double dx = p1.x - p2.x;
+      double dy = p1.y - p2.y;
+      return Math.sqrt(dx*dx + dy*dy);
+   }
+
+   // returns a String representation of this point
    public String toString(){
       return "(" + this.x + ", " + this.y +")";
    }
 }
-
---
+++ src/PointMain.java
@@ -0,0 +1,39 @@
+public class PointMain {
+    public static void main(String[] args)
+    {
+        // creates a Point object using the default constructor
+        Point p1 = new Point();
+        System.out.println("p1" + p1 + ": created using the default constructor:");
+
+        // creates another Point object using the parametric constructor
+        Point p2 = new Point(2, 3);
+        System.out.println("p2" + p2 + ": created using the parametric constructor (x=2, y=3)");
+
+        // sets p1's x-coordinate to 1
+        p1.setX(1);
+        System.out.println("p1" + p1 + ": x-coordinate is changed to 1 using command 'p1.setX(1)'");
+
+        // sets p1's new location to x=3, y=5
+        p1.setLocation(3, 5);
+        System.out.println("p1" + p1 + ": p1's location is changed to (3, 5) using command 'p1.setLocation(3, 5)'");
+
+        // sets p2's new location to x=-2, y=1
+        Point.setLocationS(p2,-2, 1);
+        System.out.println("p2" + p2 + ": p2's location is changed to (-2, 1) using command 'Point.setLocationS(p2,-2, 1);'");
+
+        // shifts p2's location by dx=2, dy=1
+        p2.translate(2,1);
+        System.out.println("p2" + p2 + ": p2's location is shifted by dx=2, dy=1 using command 'p2.translate(2,1)'");
+
+        // shifts p2's location by dx=2, dy=1
+        Point.translateS(p1, 0,1);
+        System.out.println("p1" + p1 + ": p1's location is shifted by dx=0, dy=1 using command 'Point.translateS(p1, 0,1)'");
+
+        // Computes the distance between p1 and p2 using various methods:
+        System.out.println("Computes the distance between p1 and p2 using various methods:");
+        System.out.println(p1.distance(p2));
+        System.out.println(p2.distance(p1));
+        System.out.println(Point.distanceBetweenTwoPoints(p1, p2));
+        System.out.println(Point.distanceBetweenTwoPoints(p2, p1));
+    }
+}
